c4a1e5de4cdc0394a31f26854c404f53b4d36e75
Gabe Szczepanek <gabe.szczepanek@gmail.com>
2021-11-30 06:46:46 -0400

    solution for day 3

---
+++ 2020/3/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type lineEvaluator = func(line string)
+
+func doForAllInputLines(filepath string, fn lineEvaluator) {
+	// open file
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	// scan through file
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		// parse an input line
+		line := scanner.Text()
+		fn(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+type Square struct {
+	IsTree bool
+}
+
+type Map [][]Square
+
+func parseMap(inputFile string) Map {
+	result := make([][]Square, 0)
+	doForAllInputLines(inputFile, func(line string) {
+		row := []Square{}
+		splitList := strings.Split(line, "")
+		for _, s := range splitList {
+			if s == "." {
+				row = append(row, Square{IsTree: false})
+			} else if s == "#" {
+				row = append(row, Square{IsTree: true})
+			} else {
+				panic("encountered an unknown map square")
+			}
+		}
+		result = append(result, row)
+	})
+	return result
+}
+
+func generateBiggerMap(slopeMap Map, right, down int) Map {
+	yLength := len(slopeMap)
+	amountOfDowns := yLength / down
+	desiredXLength := right * amountOfDowns
+	xLength := len(slopeMap[0])
+	repeatAmount := desiredXLength / xLength
+	repeatAmount++ // safety
+	// repeat each line pattern
+	biggerMap := make([][]Square, 0)
+	for _, row := range slopeMap {
+		newRow := []Square{}
+		var amt int
+		for amt <= repeatAmount {
+			newRow = append(newRow, row...)
+			amt++
+		}
+		biggerMap = append(biggerMap, newRow)
+	}
+	return biggerMap
+}
+
+func countTrees(slopeMap Map, right, down int) int {
+	var treeCount int
+
+	var x int
+	var y int
+
+	var finished bool
+
+	for !finished {
+		// check current position for tree
+		currSquare := slopeMap[y][x]
+		if currSquare.IsTree {
+			treeCount++
+		}
+		// check if new position is valid
+		newY := y + down
+		if newY < len(slopeMap) {
+			newX := x + right
+			if newX < len(slopeMap[newY]) {
+				// if both new positions are valid move on
+				y = newY
+				x = newX
+			} else {
+				finished = true
+			}
+		} else {
+			finished = true
+		}
+	}
+
+	return treeCount
+}
+
+func main() {
+	slopeMap := parseMap("./input.txt")
+
+	slopePairs := [][]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	results := []int{}
+	for _, pair := range slopePairs {
+		right := pair[0]
+		down := pair[1]
+		biggerMap := generateBiggerMap(slopeMap, right, down)
+		treeCount := countTrees(biggerMap, right, down)
+		results = append(results, treeCount)
+	}
+	fmt.Println(results)
+	var mult int
+	for i, r := range results {
+		if i != 0 {
+			mult = mult * r
+		} else {
+			mult = r
+		}
+	}
+	fmt.Println(mult)
+}
